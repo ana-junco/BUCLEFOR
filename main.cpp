10e14fc508f83e0274c223d83cf40848061247bf
Ana Junco <anajuncoescobar@gmail.com>
2021-06-24 16:44:16 -0500

    BUCLEFOR

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
+    //Ejercicios en clases
+
     //1.Imprimir 5 veces las palabra PUCESE
     //for (int i = 0; i < 5; i++)
     for (int i = 0; i < 5; i++)

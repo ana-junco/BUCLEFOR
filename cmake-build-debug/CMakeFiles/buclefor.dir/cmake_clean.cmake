e66474c08263d4a8bfc8f115d42bf29b06195d50
Ana Junco <anajuncoescobar@gmail.com>
2021-06-24 16:19:50 -0500

    Buclefor

---
+++ cmake-build-debug/CMakeFiles/buclefor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buclefor.dir/main.cpp.obj"
+  "buclefor.exe"
+  "buclefor.exe.manifest"
+  "buclefor.pdb"
+  "libbuclefor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buclefor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
